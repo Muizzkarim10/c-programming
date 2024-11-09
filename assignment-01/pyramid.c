1edc0c9ecafc52d9a643afcd55ed18fe1d0d91ca
Muizzkarim10 <muizzkarim10@gmail.com>
2024-11-09 22:47:27 +0500

    assignment

--- assignment-01/pyramid.c
+++ assignment-01/pyramid.c
@@ -7,10 +7,10 @@ int main()
     printf("Enter the number of rows: ");
     scanf("%d", &rows);
 
-    for (i = 1; i <= rows; i++)
+    for (i = 1; i <= rows; i++) // rows
     {
         int num = i;
-        for (j = 1; j <= rows + i - 1; j++)
+        for (j = 1; j <= rows + i - 1; j++)     // cols 
         {
             if (j <= rows - i)
             {
