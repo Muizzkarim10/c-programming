71f03a7fb6b403b00bfa46e89ba7fe8471244a58
Muizzkarim10 <muizzkarim10@gmail.com>
2024-11-13 12:17:34 +0500

    pf-paper

--- mid-paper/hourglass.c
+++ mid-paper/hourglass.c
@@ -2,5 +2,41 @@
 
 int main()
 {
-    
+    int i, j, rows;
+    printf("Enter the number of rows: \n> ");
+    scanf("%d", &rows);
+
+    if (rows % 2 == 0)
+    {
+        rows++;
+    }
+
+    int spaces = 0;
+    int stars = rows;
+
+    for (i = 1; i <= rows; i++)
+    {
+        for (j = 1; j <= spaces; j++)
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= stars; j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+        if (i < (rows / 2) + 1)
+        {
+            spaces++;
+            stars -= 2;
+        }
+        else
+        {
+            spaces--;
+            stars += 2;
+        }
+    }
+
+    return 0;
 }
