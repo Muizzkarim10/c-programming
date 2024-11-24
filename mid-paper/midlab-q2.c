e75cb1a2088c8d8ad4cafb7ec552ec7c36bd1daf
Muizzkarim10 <muizzkarim10@gmail.com>
2024-11-24 16:45:54 +0500

    mid-lab

--- mid-paper/midlab-q2.c
+++ mid-paper/midlab-q2.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-    int initial_population, year = 0;
-    float arthematic_rate, geometric_rate;
+    if (argc != 4) {
+        printf("Input the values in following format. \n");
+        printf("<initial_population> <arithmetic_rate> <geometric_rate>\n");
+        return 1;
+    }
+
+    int initial_population = atoi(argv[1]);
+    float arthematic_rate = atof(argv[2]);
+    float geometric_rate = atof(argv[3]);
+
+    int year = 0;
     float average_pop = 0.0, arthematic_pop, geometric_pop;
 
-    printf("Enter the initial population (e.g., 10000): \n> ");
-    scanf("%d", &initial_population);
-
-    printf("Enter arithmetic rate (e.g., 500): \n> ");
-    scanf("%f", &arthematic_rate);
-
-    printf("Enter geometric rate (e.g., 0.1): \n> ");
-    scanf("%f", &geometric_rate);
-
-    printf("Initial Population: %d \n", initial_population);
+    printf("Initial Population: %d\n", initial_population);
     printf("Year\tGeometric\tArithmetic\tAverage\n");
 
-    while (average_pop < initial_population * 2)
-    {
+    while (average_pop < initial_population * 2) {
         year++;
         geometric_pop = initial_population * pow(1 + geometric_rate, year);
         arthematic_pop = initial_population + (arthematic_rate * year);
         average_pop = (geometric_pop + arthematic_pop) / 2;
 
-        printf("%d\t%.2f\t\t%.2f\t\t%.2f\n", year, geometric_pop, arthematic_pop, average_pop);
+        printf("%d\t%.2f\t%.2f\t%.2f\n", year, geometric_pop, arthematic_pop, average_pop);
     }
 
     return 0;
