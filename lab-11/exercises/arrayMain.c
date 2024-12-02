ea9ae686d3c53f2e9c531f54e6800e8528e4588f
Muizzkarim10 <muizzkarim10@gmail.com>
2024-12-02 16:41:13 +0500

    code Edited

--- lab-11/exercises/arrayMain.c
+++ lab-11/exercises/arrayMain.c
@@ -6,5 +6,18 @@
 
 int main(int argc, char **argv)
 {
-    
+    // seed the random number generator with the current time
+    srand(time(NULL));
+
+    int n = 10;
+    int *arr = generateRandomArray(n);
+    printArray(arr, n);
+
+    int sum = getSum(arr, n);
+    printf("sum of elements = %d\n", sum);
+    printArray(arr, n);
+
+    free(arr);
+
+    return 0;
 }
