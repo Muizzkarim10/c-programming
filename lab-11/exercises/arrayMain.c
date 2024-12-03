b71497551828971cfc9bac859caa0846382ae259
Muizzkarim10 <muizzkarim10@gmail.com>
2024-12-03 18:13:17 +0500

    code Edited

--- lab-11/exercises/arrayMain.c
+++ lab-11/exercises/arrayMain.c
@@ -13,13 +13,12 @@ int main(int argc, char **argv)
     int *arr = generateRandomArray(n);
     printArray(arr, n);
 
-    // int sum = getSum(arr, n);
-    // printf("Sum of elements = %d\n", sum);
-
-    // double mean = getMean(arr, n);
-    // printf("Mean of elements = %.2f\n", mean);
-
-    
+    printf("Sum of elements = %d\n", getSum(arr, n));
+    printf("Mean of elements = %.2f\n", getMean(arr, n));
+    printf("Minimum Number in the array : %d\n", getMin(arr, n));
+    printf("Index of the Minimum Number : %d\n", getIndexOfMin(arr, n));
+    printf("Maximum Number in the array : %d\n", getMax(arr, n));
+    printf("Index of Maximum Number : %d\n", getIndexOfMax(arr, n));
 
     return 0;
 }
