4a563f367b7d4672f974a61fc660db33e1a15e90
Muizzkarim10 <muizzkarim10@gmail.com>
2024-12-29 21:11:25 +0500

    quiz

--- joker-twin/main.c
+++ joker-twin/main.c
@@ -1,6 +1,10 @@
+// Mueez Karim
+// Laraib Ali
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <windows.h>
+#include <ctype.h>
 
 #define ROWS 6
 #define COLS 6
@@ -14,36 +18,53 @@ char letters[36] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
                     'O', 'P', 'Q', '*', '*'};
 
 int player_one = 0, player_two = 0;
+int revealed_pairs = 0;
 
 void resetBoard();
 void printBoard();
-void checkPairs(int row, int col, int turn);
+void shuffleLetters();
+int checkPairs(int row, int col, int turn);
+void endMatch();
 
 int main()
 {
-
-    resetBoard();
-
-    // fill the hidden board random letters
     srand(time(NULL));
-    int i, j;
-    for (i = 0; i < 6; i++)
+    resetBoard();
+    shuffleLetters();
+
+    // fill the cards array with shuffled letters
+    int k = 0;
+    for (int i = 0; i < 6; i++)
     {
-        for (j = 0; j < 6; j++)
+        for (int j = 0; j < 6; j++)
         {
-            cards[i][j] = letters[rand() % 36];
+            cards[i][j] = letters[k++];
         }
     }
 
     int turn = 0;
     int player_moves = 0;
+    printBoard();
+
     while (1)
     {
-        printBoard();
-        printf("Player %d: Enter the coordinate (e.g B3): \n>", turn + 1);
+        printf("Player %d: Enter the coordinate (e.g A0): \n>", turn + 1);
         char input[3];
         scanf("%s", input);
 
+        // INPUT VALIDATIONS
+        if (!isdigit(input[1]))
+        {
+            printf("Invalid input. Please enter a valid coordinate.\n");
+            continue;
+        }
+        if (!isupper(input[0]))
+        {
+            printf("Invalid input. Letter should be Uppercase (e.g A0).\n");
+            continue;
+        }
+
+        // converting input to coordinates
         int row = atoi(&input[1]);
         int col = input[0];
         switch (input[0])
@@ -73,25 +94,79 @@ int main()
 
         if (row >= 0 && row < ROWS && col >= 0 && col < COLS)
         {
-            board[row][col] = cards[row][col];
-            printBoard();
-            player_moves++;
-            if (player_moves == 2)
+            if (board[row][col] == '#')
             {
-                checkPairs(row, col, turn);
-                player_moves = 0;
-                turn = (turn + 1) % PLAYERS;
+                board[row][col] = cards[row][col];
+                printBoard();
+                int pair_found = checkPairs(row, col, turn);
+                player_moves++;
+                if (player_moves == 2)
+                {
+                    player_moves = 0;
+                    if (!pair_found)
+                    {
+                        turn = (turn + 1) % PLAYERS;
+                    }
+                }
+            }
+            else
+            {
+                printf("Invalid input. The card is already revealed.\n");
             }
         }
         else
         {
             printf("Invalid input. Please enter a valid coordinate.\n");
         }
+
+        // All pairs revealed
+        // one less pair due to joker
+        if (revealed_pairs == 17)
+        {
+            // if jokers remain at the end
+            for (int i = 0; i < ROWS; i++)
+            {
+                for (int j = 0; j < COLS; j++)
+                {
+                    if (board[i][j] == '#')
+                    {
+                        board[i][j] = cards[i][j];
+                        if (turn == 0)
+                        {
+                            player_one++;
+                        }
+                        else
+                        {
+                            player_two++;
+                        }
+                    }
+                }
+            }
+            printBoard();
+            endMatch();
+            break;
+        }
+
+        Sleep(2000);
+        system("cls");
+        printBoard();
     }
     return 0;
 }
 
-// fills the board with hastags
+// Shuffle the letters array
+void shuffleLetters()
+{
+    for (int i = 0; i < 36; i++)
+    {
+        int j = rand() % 36;
+        char temp = letters[i];
+        letters[i] = letters[j];
+        letters[j] = temp;
+    }
+}
+
+// Fill the board with '#'
 void resetBoard()
 {
     for (int i = 0; i < 6; i++)
@@ -105,8 +180,8 @@ void resetBoard()
 
 void printBoard()
 {
-    printf("Player 1 score : %d\n", player_one);
-    printf("Player 1 score : %d\n", player_two);
+    printf("   Player 1 score : %d\n", player_one);
+    printf("   Player 2 score : %d\n", player_two);
     printf("    A   B   C   D   E   F \n");
     printf("  +---+---+---+---+---+---+\n");
     for (int i = 0; i < ROWS; i++)
@@ -122,43 +197,116 @@ void printBoard()
     printf("\n");
 }
 
-void checkPairs(int row, int col, int turn)
+int checkPairs(int row, int col, int turn)
 {
     static int last_row = -1;
     static int last_col = -1;
-    static int last_turn = -1;
     static int first_reveal = 1;
+
+    char second_card = cards[row][col];
+    second_card = tolower(second_card);
+
+    char first_card = cards[last_row][last_col];
+    first_card = tolower(first_card);
 
     if (first_reveal == 1)
     {
         last_col = col;
         last_row = row;
-        last_turn = turn;
         first_reveal = 0;
+        return 0; // No pair found yet
     }
     else
     {
-        if (cards[last_row][last_col] == cards[row][col])
+        if (first_card == '*' || second_card == '*')
         {
-            printf("Match Found!");
-            cards[last_row][last_col] = ' ';
-            cards[row][col] = ' ';
+            printf("Joker revealed!\n");
+            revealed_pairs++;
+            // removes joker and other card
+            board[last_row][last_col] = ' ';
+            board[row][col] = ' ';
 
-            if (turn == 0)
+            // removes the pair of other card
+            char other_card;
+            if (first_card == '*')
             {
-                player_one++;
+                other_card = second_card;
             }
             else
             {
-                player_two++;
+                other_card = first_card;
             }
+
+            for (int i = 0; i < ROWS; i++)
+            {
+                for (int j = 0; j < COLS; j++)
+                {
+                    if (tolower(cards[i][j]) == other_card)
+                    {
+                        board[i][j] = ' ';
+                    }
+                }
+            }
+
+            if (turn == 0)
+            {
+                player_one += 2;
+            }
+            else
+            {
+                player_two += 2;
+            }
+            first_reveal = 1;
+            return 1; // Pair found
         }
         else
         {
-            printf("No Match Found!");
-            board[last_row][last_col] = '#';
-            board[row][col] = '#';
+            if (first_card == second_card)
+            {
+                revealed_pairs++;
+                board[last_row][last_col] = ' ';
+                board[row][col] = ' ';
+                printf("Pairs Revealed!\n");
+
+                if (turn == 0)
+                {
+                    player_one++;
+                }
+                else
+                {
+                    player_two++;
+                }
+                first_reveal = 1;
+                return 1; // Pair found
+            }
+            else
+            {
+                printf("No Match Found!\n");
+                board[last_row][last_col] = '#';
+                board[row][col] = '#';
+                first_reveal = 1;
+                return 0; // No pair found
+            }
         }
-        first_reveal = 1;
+    }
+}
+
+void endMatch()
+{
+    printf("Game Over!\n");
+    printf("Player 1 score: %d\n", player_one);
+    printf("Player 2 score: %d\n", player_two);
+
+    if (player_one > player_two)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (player_two > player_one)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("It's a tie!\n");
     }
 }
