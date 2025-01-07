7478017b5fc37310841b235226a597206b60bab4
Muizzkarim10 <muizzkarim10@gmail.com>
2025-01-07 23:12:56 +0500

    Calculator

---
+++ calculator/calculator.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <math.h>
+
+int main()
+{
+    int a = 0, b = 0;
+    int choice = 0;
+    while (1)
+    {
+        printf("Please select one of the following: \n");
+        printf("1. Addition.\n");
+        printf("2. Subtraction.\n");
+        printf("3. Multiplication.\n");
+        printf("4. Division.\n");
+        printf("5. Power.\n");
+        printf("6. Modulus.\n");
+        printf("0. Exit.\n");
+        printf("Your choice: ");
+        if (scanf("%d", &choice) != 1)
+        {
+            printf("Invalid input. \nExiting...");
+            return 0;
+        }
+
+        if (choice == 0)
+        {
+            printf("Exiting...\n");
+            return 0;
+        }
+
+        printf("Enter the First number: ");
+        if (scanf("%d", &a) != 1)
+        {
+            printf("\nInvalid input. Please Enter a number.");
+            return 0;
+        }
+
+        printf("Enter the Second number: ");
+        if (scanf("%d", &b) != 1)
+        {
+            printf("\nInvalid input. Please Enter a number.");
+            return 0;
+        }
+
+        switch (choice)
+        {
+        case 1:
+            printf("Result: %d + %d = %d\n", a, b, a + b);
+            break;
+
+        case 2:
+            printf("Result: %d - %d = %d\n", a, b, a - b);
+            break;
+
+        case 3:
+            printf("Result: %d * %d = %d\n", a, b, a * b);
+            break;
+
+        case 4:
+            printf("Result: %d / %d = %d\n", a, b, a / b);
+            break;
+
+        case 5:
+            printf("Result: %d^%d = %.0f", a, b, pow(a, b));
+            break;
+
+        case 6:
+            printf("Result: %d %% %d = %d", a, b, a % b);
+            break;
+
+        default:
+            printf("Invalid choice!\n");
+            break;
+        }
+    }
+    printf("\n");
+    printf("\n");
+
+    return 0;
+}
