b9339de6b17058480ac10036cb452ae804f4ed1e
Muizzkarim10 <muizzkarim10@gmail.com>
2025-01-07 23:52:41 +0500

    Modified output.

--- calculator/calculator.c
+++ calculator/calculator.c
@@ -2,21 +2,26 @@
 #include <ctype.h>
 #include <math.h>
 
+int menu()
+{
+    printf("Please select one of the following: \n");
+    printf("1. Addition.\n");
+    printf("2. Subtraction.\n");
+    printf("3. Multiplication.\n");
+    printf("4. Division.\n");
+    printf("5. Power.\n");
+    printf("6. Modulus.\n");
+    printf("0. Exit.\n");
+    printf("Your choice: ");
+}
+
 int main()
 {
     int a = 0, b = 0;
     int choice = 0;
     while (1)
     {
-        printf("Please select one of the following: \n");
-        printf("1. Addition.\n");
-        printf("2. Subtraction.\n");
-        printf("3. Multiplication.\n");
-        printf("4. Division.\n");
-        printf("5. Power.\n");
-        printf("6. Modulus.\n");
-        printf("0. Exit.\n");
-        printf("Your choice: ");
+        menu();
         if (scanf("%d", &choice) != 1)
         {
             printf("Invalid input. \nExiting...");
@@ -43,22 +48,24 @@ int main()
             return 0;
         }
 
+        printf("+-------------------------+\n");
+
         switch (choice)
         {
         case 1:
-            printf("Result: %d + %d = %d\n", a, b, a + b);
+            printf("Result: %d + %d = %d", a, b, a + b);
             break;
 
         case 2:
-            printf("Result: %d - %d = %d\n", a, b, a - b);
+            printf("Result: %d - %d = %d", a, b, a - b);
             break;
 
         case 3:
-            printf("Result: %d * %d = %d\n", a, b, a * b);
+            printf("Result: %d * %d = %d", a, b, a * b);
             break;
 
         case 4:
-            printf("Result: %d / %d = %d\n", a, b, a / b);
+            printf("Result: %d / %d = %d", a, b, a / b);
             break;
 
         case 5:
@@ -73,9 +80,9 @@ int main()
             printf("Invalid choice!\n");
             break;
         }
+        printf("\n+-------------------------+\n");
+        printf("\n");
     }
-    printf("\n");
-    printf("\n");
 
     return 0;
 }
