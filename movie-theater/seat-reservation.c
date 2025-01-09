0b1883a46c57bd27898db571b252661d3cb1b78c
Muizzkarim10 <muizzkarim10@gmail.com>
2025-01-09 20:15:57 +0500

    seat-reservation-system

---
+++ movie-theater/seat-reservation.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+
+void printBoard();
+
+char seats[10][10];
+
+int main()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            seats[i][j] = 'o';
+        }
+    }
+    printBoard();
+}
+
+void printBoard()
+{
+    printf("+--------------+\n");
+    printf("| Available: o |\n| Reserved: x  |\n");
+    printf("+--------------+\n");
+    printf("    A   B   C   D   E   F   G   H   I   J\n");
+    printf("  +---+---+---+---+---+---+---+---+---+---+\n");
+    for (int i = 0; i < 9; i++)
+    {
+        printf("%d |", i + 1);
+        for (int j = 0; j < 10; j++)
+        {
+            printf(" %c |", seats[i][j]);
+        }
+        printf("\n");
+        printf("  +---+---+---+---+---+---+---+---+---+---+\n");
+    }
+    printf("\n");
+}
